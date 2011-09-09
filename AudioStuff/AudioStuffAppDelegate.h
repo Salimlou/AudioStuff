2bb78d8363fc07e46dd85cc3ec70eb226cfa0e6c
Salim <benabbousalim@gmail.com>
2011-09-09 21:05:15 +0000

    Initial Commit

---
+++ AudioStuff/AudioStuffAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AudioStuffAppDelegate.h
+//  AudioStuff
+//
+//  Created by emsi on 09/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AudioStuffAppDelegate : NSObject <NSApplicationDelegate> {
+@private
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ AudioStuff/AudioStuffAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AudioStuffAppDelegate.m
+//  AudioStuff
+//
+//  Created by emsi on 09/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AudioStuffAppDelegate.h"
+
+@implementation AudioStuffAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
